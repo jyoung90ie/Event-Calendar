ab66ca5a6ecb2c532aaf033af51bad517f8757ca
jyoung90ie <jyoung90ie@gmail.com>
2019-11-21 11:02:58 +0000

    Basic flask routing added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template
 from dotenv import load_dotenv
+import pymongo
 
 # get environment variables
 load_dotenv()
@@ -9,9 +10,29 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/trips/')
+@app.route('/trips/all/')
 def index():
-    return 'test'
-    # return render_template("index.html")
+    return render_template('index.html')
+
+# trips functionality
+
+
+@app.route('/trip/new/')
+def trip_new():
+    return render_template('new_trip.html')
+
+
+@app.route('/trip/detailed/')
+# @app.route('/trip/<trip_id>/detailed/')
+def trip_detailed():
+    return render_template('trip_detailed.html')
+
+
+@app.route('/stop/new/')
+# @app.route('/trip/<trip_id>/stop/new/')
+def trip_new_stop():
+    return render_template('trip_detailed.html')
 
 
 if __name__ == '__main__':
